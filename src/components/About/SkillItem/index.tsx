aa39f907b6166fdca865718f29c2d358fa812a9b
nivaldoandrade <nivaldoandradef@gmail.com>
2022-02-14 10:34:25 -0300

    add: comp skillBar animation

--- src/components/About/SkillItem/index.tsx
+++ src/components/About/SkillItem/index.tsx
@@ -1,11 +1,31 @@
+import { motion, useAnimation } from 'framer-motion';
+import { useEffect } from 'react';
+import { useInView } from 'react-intersection-observer';
 import styles from './styles.module.scss';
 
 interface SkillItemProps {
   title: string;
   percentage: number;
+  delay?: number;
 }
 
-export function SkillItem({ title, percentage }: SkillItemProps) {
+export function SkillItem({ title, percentage, delay }: SkillItemProps) {
+  const controls = useAnimation();
+  const [ref, inView] = useInView();
+
+  const variants = {
+    hidden: { width: 0 },
+    visible: {
+      width: `${percentage}%`,
+    },
+  };
+
+  useEffect(() => {
+    if (inView) {
+      controls.start('visible');
+    }
+  }, [controls, inView]);
+
   return (
     <div className={styles.skillContent}>
       <div className={styles.skillHeader}>
@@ -13,9 +33,19 @@ export function SkillItem({ title, percentage }: SkillItemProps) {
         <span>{percentage} %</span>
       </div>
       <div className={styles.skillBar}>
-        <div
+        <motion.div
+          ref={ref}
           className={styles.skillBarFill}
           style={{ width: `${percentage}%` }}
+          animate={controls}
+          initial="hidden"
+          variants={variants}
+          transition={{
+            type: 'tween',
+            ease: 'easeInOut',
+            duration: 1,
+            delay,
+          }}
         />
       </div>
     </div>
