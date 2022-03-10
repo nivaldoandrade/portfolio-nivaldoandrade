279d146dc9586feec3e1c535ff0fa6578f542983
nivaldoandrade <nivaldoandradef@gmail.com>
2022-03-10 10:03:24 -0300

    emailjs installation and configuration

--- src/components/Contact/index.tsx
+++ src/components/Contact/index.tsx
@@ -1,3 +1,5 @@
+import { FormEvent, useCallback, useState } from 'react';
+import emailjs from 'emailjs-com';
 import { BiMailSend } from 'react-icons/bi';
 import { FaWhatsapp } from 'react-icons/fa';
 import { BsLinkedin } from 'react-icons/bs';
@@ -7,6 +9,54 @@ import { Card } from './Card';
 import styles from './styles.module.scss';
 
 export function Contact() {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [message, setMessage] = useState('');
+  const [formSubmitted, setFormSubmitted] = useState({ title: '' });
+
+  const handleSubmit = useCallback(
+    (e: FormEvent) => {
+      e.preventDefault();
+
+      const data = {
+        name,
+        email,
+        message,
+      };
+
+      setFormSubmitted({ title: 'Enviando...' });
+      emailjs
+        .send(
+          process.env.NEXT_PUBLIC_EMAILJS_SERVICEID,
+          process.env.NEXT_PUBLIC_EMAILJS_TEMPLATEID,
+          data,
+          process.env.NEXT_PUBLIC_EMAILJS_USERID,
+        )
+        .then(
+          ({ status }) => {
+            if (status === 200) {
+              setName('');
+              setEmail('');
+              setMessage('');
+              setFormSubmitted({ title: 'Enviado' });
+            } else {
+              setFormSubmitted({ title: '' });
+            }
+          },
+          err => {
+            console.log(err);
+          },
+        );
+    },
+    [name, email, message, setFormSubmitted],
+  );
+
+  setTimeout(() => {
+    if (formSubmitted.title === 'Enviado') {
+      setFormSubmitted({ title: '' });
+    }
+  }, 4000);
+
   return (
     <section className={styles.contactContainer} id="contact">
       <span>Entre em</span>
@@ -35,20 +85,47 @@ export function Contact() {
         </div>
         <div className={styles.formContent}>
           <h2>Fale Comigo</h2>
-          <form>
+          <form onSubmit={handleSubmit}>
             <div className={styles.input}>
               <label htmlFor="name">Nome</label>
-              <input type="text" placeholder="Insira seu nome" id="name" />
+              <input
+                type="text"
+                placeholder="Insira seu nome"
+                id="name"
+                value={name}
+                onChange={e => setName(e.target.value)}
+                required
+              />
             </div>
             <div className={styles.input}>
               <label htmlFor="email">Email</label>
-              <input type="text" placeholder="Insira seu email" id="email" />
+              <input
+                type="text"
+                placeholder="Insira seu email"
+                id="email"
+                value={email}
+                onChange={e => setEmail(e.target.value)}
+                required
+              />
             </div>
             <div className={styles.input}>
               <label htmlFor="project">Mensagem</label>
-              <textarea placeholder="Escreva sua mensagem" id="project" />
+              <textarea
+                placeholder="Escreva sua mensagem"
+                id="project"
+                value={message}
+                onChange={e => setMessage(e.target.value)}
+                required
+              />
             </div>
-            <button type="submit">Enviar Mensagem</button>
+            <button type="submit">
+              {formSubmitted.title === 'Enviando...'
+                ? formSubmitted.title
+                : 'Enviar Mensagem'}
+            </button>
+            {formSubmitted.title === 'Enviado' && (
+              <p>Mensagem enviado com sucesso!</p>
+            )}
           </form>
         </div>
       </div>
