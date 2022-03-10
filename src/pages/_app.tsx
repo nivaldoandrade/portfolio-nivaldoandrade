564c552a2b979feafd3a4db54f0702cb794488e0
nivaldoandrade <nivaldoandradef@gmail.com>
2022-03-10 11:23:39 -0300

    add favicon / apply SEO/HTML META and title

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -8,6 +8,7 @@ function MyApp({ Component, pageProps }: AppProps) {
     <>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <title>Nivaldo Andrade - Desenvolvedor Frontend</title>
       </Head>
       <Component {...pageProps} />
     </>
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,7 @@ import Document, { Html, Head, Main, NextScript } from 'next/document';
 export default class MyDocumento extends Document {
   render() {
     return (
-      <Html>
+      <Html lang="pt-br">
         <Head>
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link
@@ -15,6 +15,21 @@ export default class MyDocumento extends Document {
             href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600&display=swap"
             rel="stylesheet"
           />
+          <link rel="icon" type="image/x-icon" href="/favicon.ico" />
+          <meta charSet="UTF-8" />
+          <meta
+            name="description"
+            content="Nivaldo Andrade é desenvolvedor front end com foco na stack javascript/typescript, principalmente com ReactJS e Next.js. Portfólio para exibição dos projetos"
+          />
+          <meta
+            name="keywords"
+            content="desenvolvedor, front end, react.js, reactjs, next.js,react native,nodejs, javascript, typescript, html, css, web development, learn web development, front end development"
+          />
+          <meta
+            name="author"
+            content="Nivaldo Andrade<nivaldoandradef@gmail.com>"
+          />
+          <meta name="theme-color" content="#0b111e" />
         </Head>
         <body>
           <Main />
