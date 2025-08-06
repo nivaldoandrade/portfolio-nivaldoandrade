9b10ada21e57db15bd2b466b0865c6c12a75c642
nivaldoandrade <nivaldoandradef@gmail.com>
2025-08-06 13:35:36 -0300

    feat: add google analytics integration

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import { AppProps } from 'next/app';
 import Head from 'next/head';
+import Script from 'next/script';
 
 import '../styles/globals.scss';
 
@@ -8,8 +9,22 @@ function MyApp({ Component, pageProps }: AppProps) {
     <>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <title>Nivaldo Andrade - Desenvolvedor Frontend</title>
+        <title>Nivaldo Andrade - Desenvolvedor Frontend e Backend</title>
       </Head>
+      <Script
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){
+                        window.dataLayer.push(arguments);
+                    }
+                    gtag('js', new Date());
+                    gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}');
+                `}
+      </Script>
 
       <Component {...pageProps} />
     </>
